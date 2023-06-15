ac56337e48dd85e52a32b82c92c746830b201106
hamidhanaf <hanafhamid@hotmail.fr>
2023-06-15 16:44:47 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main (void){
+
+/**
+ * main entry point 
+ * Description : c program that printput function
+ * Return : 0 successful
+ */
+
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle:");
 	return (0);
 }
